e25003acbb13063de045b43c180078212cac8d6a
Ajit Kumar Dubey <134035235+tech-ajit-mob@users.noreply.github.com>
2023-10-08 11:52:49 +0530

    Add files via upload
    src added

---
+++ src/main/java/com/jenkin/simpli/JenkinsSimpli/JenkinsSimpliApplication.java
@@ -0,0 +1,13 @@
+package com.jenkin.simpli.JenkinsSimpli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSimpliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSimpliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkin/simpli/JenkinsSimpli/JenkinsSimpliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkin.simpli.JenkinsSimpli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSimpliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
